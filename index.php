45b3a4d13cf58fe18d4be96372df64f1920bb398
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-05 18:02:56 +0100

    new

--- index.php
+++ index.php
@@ -5,17 +5,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 
 <body>
     <h3>Signup</h3>
 
-    <form action="includes/" method="post"></form>
-    <input type="text" name="username" placeholder="Username">
-    <input type="password" name="pwd" placeholder="Password">
-    <input type="text" name="email" placeholder="Email">
-    <button>Sign Up</button>
+    <form action="includes/" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="pwd" placeholder="Password">
+        <input type="text" name="email" placeholder="Email">
+        <button>Sign Up</button>
+    </form>
 </body>
 
 </html>
