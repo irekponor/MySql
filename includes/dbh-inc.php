c69178b17940364007fd6baf078d8f07db6e665c
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-05 17:09:00 +0100

    new

--- includes/dbh-inc.php
+++ includes/dbh-inc.php
@@ -3,3 +3,10 @@
 $dsn = "mysql:host=localhost;dbname=buisness";
 $dbusername = "root";
 $dbpassword = "";
+
+try {
+    $pdo = new PDO($dsn, $dbusername, $dbpassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "connection fucking failed bitch:" . $e->getMessage();
+}
