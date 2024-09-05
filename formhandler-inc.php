a7a62e53795b02fbc46ec3b57b71518e491c9eb7
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-05 23:18:10 +0100

    edit

--- formhandler-inc.php
+++ formhandler-inc.php
@@ -1,6 +1,15 @@
 <?php
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $pwd = $_POST["pwd"];
+    $email = $_POST["email"];
+
+    try {
+        require_once "dbh-inc.php";
+    } catch (PDOException $e) {
+        die("Query Failed:" . $e->getMessage());
+    }
 } else {
     header("Location: index.php");
 }
