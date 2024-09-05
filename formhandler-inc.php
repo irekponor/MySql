86f73637e65f5028c82c716ec06a166f3e9c7e3c
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-05 23:29:34 +0100

    edit

--- formhandler-inc.php
+++ formhandler-inc.php
@@ -7,6 +7,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     try {
         require_once "dbh-inc.php";
+
+        $query = "INSERT INTO users (username, pwd, email) VALUES (?, ?, ?);";
+
+        $stmt = $pdo->prepare($query);
+
+        $stmt->execute([$username, $pwd, $email]);
+
+        $pdo = null;
+        $stmt = null;
+
+        header("Location: index.php");
+
+        die();
     } catch (PDOException $e) {
         die("Query Failed:" . $e->getMessage());
     }
