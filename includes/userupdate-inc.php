601f4e00b0edaf8a578d881f0cdf1008988b961a
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-06 02:32:10 +0100

    folder

---
+++ includes/userupdate-inc.php
@@ -0,0 +1,32 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $pwd = $_POST["pwd"];
+    $email = $_POST["email"];
+
+    try {
+        require_once "includes/dbh-inc.php";
+
+        $query = "UPDATE users SET username = :username, pwd = :pwd, email = :email WHERE id = 2;";
+
+        $stmt = $pdo->prepare($query);
+
+        $stmt->bindParam(":username", $username);
+        $stmt->bindParam(":pwd", $pwd);
+        $stmt->bindParam(":email", $email);
+
+        $stmt->execute();
+
+        $pdo = null;
+        $stmt = null;
+
+        header("Location: index.php");
+
+        die();
+    } catch (PDOException $e) {
+        die("Query Failed:" . $e->getMessage());
+    }
+} else {
+    header("Location: index.php");
+}
